34238c1e2dac20380b49cb84ea9a215013ecd791
Albert Boada <boa.intrance@gmail.com>
2014-04-14 00:30:16 +0200

    Implemented basic "Stats by Week" for each Project (refs #1) (needs DRY refactor)

--- app/models/project.rb
+++ app/models/project.rb
@@ -133,7 +133,6 @@ class Project < ActiveRecord::Base
         self.logs.each_with_index do |log, i|
             curr_log = log # save log for outside the loop. when loop ends, should contain last log of all
             log_start_month = log.start.strftime("%Y-%m")
-            puts log_start_month
 
             if i == 0
                 curr_month = log_start_month
@@ -196,6 +195,91 @@ class Project < ActiveRecord::Base
             else # hack if last log is still not finished so we can know the last day is still open
                 now = Time.now.change(:usec => 0)
                 _log.start  = now - total_spent_time_for_curr_month
+            end
+            stats << _log
+        end
+
+        stats
+    end
+
+    #
+    # In order to properly calculate a Project's Week Statistics, this function assumes that:
+    #     - Project's Logs are sorted by `start` date, from oldest to newest
+    #     - There is NO OVERLAPPING between Logs, which is ensured by Log model's validations
+    #
+    def weekstats
+        stats = []
+
+        curr_week                      = nil
+        curr_log                       = nil
+        start_datetime_for_curr_week   = nil # important that we keep the exact start date of each day's first log
+        total_spent_time_for_curr_week = 0
+
+        self.logs.each_with_index do |log, i|
+            curr_log = log # save log for outside the loop. when loop ends, should contain last log of all
+            log_start_week = log.start.strftime("%Y-%V")
+
+            if i == 0
+                curr_week = log_start_week
+                start_datetime_for_curr_week = log.start
+            end
+
+            #
+            # If day-change, add the previously fully-calculated day-stats to the
+            # `stats` array, before starting with the new-day stats calculations
+            #
+            if log_start_week != curr_week
+                _log = Log.new
+                _log.start  = start_datetime_for_curr_week
+                _log.finish = _log.start + total_spent_time_for_curr_week
+
+                stats << _log
+
+                #
+                # Init new day calculations
+                #
+                curr_week = log_start_week
+                start_datetime_for_curr_week = log.start
+                total_spent_time_for_curr_week = 0
+            end
+
+            #if date != log.finish.to_date #logs spanning more than 1 day
+            #  _time = 0
+            #  start_datetime  = log.start
+            #  finish_datetime = log.start.to_date.to_time
+            #  while finish_datetime != log.finish
+            #    _time = finish_datetime - start_datetime
+            #
+            #    _log = Log.new
+            #    _log.start  = start_datetime
+            #    _log.finish = finish_datetime
+            #
+            #    stats << _log
+            #
+            #    start_datetime  = finish_datetime
+            #    finish_datetime = finish_datetime + 1.days
+            #    if finish_datetime > log.finish
+            #      finish_datetime = log.finish
+            #    end
+            #  end
+            #else
+            total_spent_time_for_curr_week += log.spent
+            #end
+        end
+
+        last_log = curr_log
+
+        #
+        # Add the last day-stats to the `stats` array
+        #
+        if last_log
+            _log = Log.new
+            if last_log.finish
+                _log.start  = start_datetime_for_curr_week
+                _log.finish = _log.start + total_spent_time_for_curr_week
+            else # hack if last log is still not finished so we can know the last day is still open
+                now = Time.now.change(:usec => 0)
+                _log.start  = now - total_spent_time_for_curr_week
             end
             stats << _log
         end
